ceae81bcaaa12bb4ebc58a4f14c786d4184a778e
Chenxi-Liu <329052927@qq.com>
2021-10-20 14:13:42 +0800

    feat: socket.io

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-19 11:51:52
- * @LastEditTime: 2021-10-20 13:41:34
+ * @LastEditTime: 2021-10-20 14:12:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \egg-colloection\app\router.js
@@ -16,6 +16,6 @@ module.exports = app => {
   router.get('/', controller.home.index)
 
   // socket.io
-  // ws://localhost:7001
+  // 路由负责将 socket 连接的不同 events 分发到对应的 controller ws://localhost:7001/
   io.of('/').route('index', io.controller.nsp.index)
 }
